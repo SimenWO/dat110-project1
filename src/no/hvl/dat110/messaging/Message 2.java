df9a0c5a5bc0641b9f44660b9338075043cd2131
Simen Østensen <simen.ostensen@gmail.com>
2019-01-31 10:47:22 +0100

    Message - encasulate

---
+++ src/no/hvl/dat110/messaging/Message 2.java
@@ -0,0 +1,58 @@
+package no.hvl.dat110.messaging;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static no.hvl.dat110.messaging.MessageConfig.MESSAGINGPORT;
+import static no.hvl.dat110.messaging.MessageConfig.SEGMENTSIZE;
+
+public class Message {
+
+	private byte[] payload;
+
+	// check for length within boundary
+	public Message(byte[] payload) {
+		if (payload.length > SEGMENTSIZE) {
+			this.payload = payload;
+		} else {
+			System.out.println("The messeage is to long");
+		}
+
+	}
+
+	public Message() {
+		super();
+	}
+
+	public byte[] getData() {
+		return this.payload;
+	}
+
+	public byte[] encapsulate() {
+
+		byte[] encoded = new byte[MessageConfig.SEGMENTSIZE];
+
+		encoded[0] = (byte) this.payload.length;
+
+		for (int i = 0; i < this.payload.length; i++) {
+			encoded[i + 1] = this.payload[i];
+		}
+
+		return encoded;
+
+	}
+		public void decapsulate( byte[] received){
+
+			// TODO
+			// decapsulate data in received and put in payload
+
+			throw new RuntimeException("not yet implemented");
+
+		}
+	}
+
