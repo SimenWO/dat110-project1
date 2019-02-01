c24ab20dc15094c20b3d16eb16e97a25ea7e2433
Simen Østensen <simen.ostensen@gmail.com>
2019-02-01 14:03:28 +0100

    controller.java
    Testing after this push

--- src/no/hvl/dat110/system/controller/Controller.java
+++ src/no/hvl/dat110/system/controller/Controller.java
@@ -3,52 +3,44 @@ package no.hvl.dat110.system.controller;
 import no.hvl.dat110.rpc.RPCClient;
 import no.hvl.dat110.rpc.RPCServerStopStub;
 
-public class Controller  {
-	
+public class Controller {
+
 	private static int N = 5;
-	
-	public static void main (String[] args) {
-		
-		Display display;
-		Sensor sensor;
-		
-		RPCClient displayclient,sensorclient;
-		
-		System.out.println("Controller starting ...");
-				
+
+	public static void main(String[] args) {
+
+		Display display = new Display();
+		Sensor sensor = new Sensor();
+		RPCClient displayclient = new RPCClient(Common.DISPLAYHOST, Common.DISPLAYPORT);
+		RPCClient sensorclient = new RPCClient(Common.SENSORHOST, Common.SENSORPORT);
 		RPCServerStopStub stopdisplay = new RPCServerStopStub();
 		RPCServerStopStub stopsensor = new RPCServerStopStub();
-		
-		displayclient = new RPCClient(Common.DISPLAYHOST,Common.DISPLAYPORT);
-		sensorclient = new RPCClient(Common.SENSORHOST,Common.SENSORPORT);
-		
-		// TODO
-		// create display and sensor object
-		// create RPC clients for display device and sensor device
-		// register RPC methods in the RPC layer
-		
-		if (true) {
-			  throw new RuntimeException("not yet implemented");
-		}
-		
-		// register stop methods in the RPC middleware
+
+		System.out.println("Controller starting ...");
+
+		displayclient.register(display);
+		sensorclient.register(sensor);
+
 		displayclient.register(stopdisplay);
 		sensorclient.register(stopsensor);
-		
-		// TODO:
-		// loop while reading from sensor and write to display via RPC
-		
-		if (true) {
-			  throw new RuntimeException("not yet implemented");
+
+		try {
+			while (true) {
+				int value = sensor.read();
+				display.write(value+"°C");
+				return;
 			}
-		
+		} catch (Exception e) {
+			System.out.println("Error occurred: " + e);
+		}
+
 		stopdisplay.stop();
 		stopsensor.stop();
-	
+
 		displayclient.disconnect();
 		sensorclient.disconnect();
-		
+
 		System.out.println("Controller stopping ...");
-		
+
 	}
 }
